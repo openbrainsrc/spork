ae221ce1f6039ea4f0f61fafdd2eecf0a2f16778
Tom Feron <tho.feron@gmail.com>
2014-10-28 11:21:09 +0000

    #1 - Changes in the migration tool of findmelike

--- spork-migrate/src/Migrate.hs
+++ spork-migrate/src/Migrate.hs
@@ -4,20 +4,31 @@ import Control.Monad
 
 import Data.List
 import Data.String
+import Data.Time
 
 import System.Directory
 import System.Environment
 import System.Exit
 import System.FilePath
 import System.IO
+import System.Locale
 
 import Spork.Config
 import Spork.Database
 import Spork.DatabaseConfig
 
 main :: IO ()
-main = dbFromArgs $ \_ -> do
-  alreadyMigratedTimestamps <- getAlreadyMigratedTimestamps
+main = dbFromArgs $ \args -> case args of
+  "run" : _        -> runMigrations
+  "new" : name : _ -> createMigrationFile name
+  _ -> liftIO $ do
+    hPutStrLn stderr
+              "Usage: spork-migrate config-file (run | new migration_name)"
+    exitFailure
+
+runMigrations :: DBC OnlyDatabaseConfig ()
+runMigrations = do
+  alreadyMigratedNames <- getAlreadyMigratedNames
 
   OnlyDatabaseConfig conf <- getConf
   let migrationsPath = maybe "migrations" id $ migrations_directory conf
@@ -25,22 +36,32 @@ main = dbFromArgs $ \_ -> do
 
   let toMigrate = sortBy (\(a,_) (b,_) -> compare a b)
                 . filter ((".sql" `isSuffixOf`) . snd)
-                . filter ((`notElem` alreadyMigratedTimestamps) . fst)
+                . filter ((`notElem` alreadyMigratedNames) . fst)
                 . map (\n -> (takeWhile (`elem` "0123456789") n
                              , migrationsPath </> n))
                 $ fileNames
 
-  forM_ toMigrate $ \(t,p) -> do
+  forM_ toMigrate $ \(n,p) -> do
     content <- liftIO $ readFile p
-    let qStr = content ++ "; INSERT INTO migrations ( timestamp ) VALUES ('"
-                       ++ t ++ "')"
+    let qStr = content ++ "; INSERT INTO migrations ( name ) VALUES ('"
+                       ++ n ++ "')"
         q    = fromString qStr
     executeDB_ q
 
-getAlreadyMigratedTimestamps :: DBC conf [String]
-getAlreadyMigratedTimestamps = do
+getAlreadyMigratedNames :: DBC conf [String]
+getAlreadyMigratedNames = do
   _ <- executeDB_ $ fromString $ "CREATE TABLE IF NOT EXISTS migrations ("
-                                 ++ " timestamp varchar(12) NOT NULL )"
+                                 ++ " name varchar(12) PRIMARY KEY )"
 
-  rows <- queryDB_ "SELECT timestamp FROM migrations"
+  rows <- queryDB_ "SELECT name FROM migrations"
   return $ map head rows
+
+createMigrationFile :: String -> DBC OnlyDatabaseConfig ()
+createMigrationFile name = do
+  OnlyDatabaseConfig conf <- getConf
+  let migrationsDir = maybe "migrations" id $ migrations_directory conf
+  now <- liftIO $ getCurrentTime
+  let prefix = formatTime defaultTimeLocale "%Y%m%d%H%M%S" now
+      name   = prefix ++ "_" ++ name <.> "sql"
+      path   = migrationsDir </> name
+  liftIO $ writeFile path "-- Enter the SQL queries to execute here."
