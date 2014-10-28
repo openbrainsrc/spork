f7dab231af6dfb9580577ad756af07fcdaf08426
Tom Nielsen <tanielsen@gmail.com>
2014-10-28 09:57:47 +0000

    mv databaseconfig  to own file

--- spork-lib/Spork/Database.hs
+++ spork-lib/Spork/Database.hs
@@ -1,10 +1,7 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveGeneric, GeneralizedNewtypeDeriving #-}
 
 module Spork.Database
-  ( DatabaseConfig,
-    createConn,
-    destroyConn,
-    readField,
+  ( readField,
     mReadField,
     mBinaryField,
     DB,
@@ -45,42 +42,6 @@ import Database.PostgreSQL.Simple.FromRow
 import Control.Monad.Reader
 import Control.Monad (void)
 import qualified Data.Binary as B
-
-
-data OnlyDatabaseConfig = OnlyDatabaseConfig { database :: DatabaseConfig } deriving Generic
-
-data DatabaseConfig = DatabaseConfig
-  { user                   :: String
-  , password               :: String
-  , host                   :: String
-  , port                   :: Integer
-  , dbname                 :: String
-  , migrations_directory   :: Maybe String
-  } deriving (Show, Eq, Generic)
-
-instance FromJSON DatabaseConfig
-instance FromJSON OnlyDatabaseConfig
-
-createConn :: DatabaseConfig -> IO Connection
-createConn config = do
-   catch (createConn' config)
-         (\(_::SomeException) -> do putStrLn "Failed to connecto to database, retrying in 10s.."
-                                    threadDelay $ 10 * 1000 * 1000
-                                    createConn' config)
-
-
-createConn' :: DatabaseConfig -> IO Connection
-createConn' config = do
-  connect ConnectInfo
-    { connectHost     = host     config
-    , connectUser     = user     config
-    , connectPassword = password config
-    , connectDatabase = dbname   config
-    , connectPort     = fromInteger $ port config
-    }
-
-destroyConn :: Connection -> IO ()
-destroyConn = close
 
 newtype DB a = DB { unDB :: ReaderT Connection IO a }
   deriving (Monad, Functor, MonadIO)
---
+++ spork-lib/Spork/DatabaseConfig.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveGeneric, GeneralizedNewtypeDeriving #-}
+
+module Spork.DatabaseConfig
+  ( DatabaseConfig,
+    OnlyDatabaseConfig,
+    createConn,
+    destroyConn
+  ) where
+
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad
+
+import           Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
+
+import           System.Exit
+import           System.IO
+
+import           GHC.Conc
+import GHC.Generics
+
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+
+import Control.Monad.Reader
+import Control.Monad (void)
+import qualified Data.Binary as B
+
+
+data OnlyDatabaseConfig = OnlyDatabaseConfig { database :: DatabaseConfig } deriving Generic
+
+data DatabaseConfig = DatabaseConfig
+  { user                   :: String
+  , password               :: String
+  , host                   :: String
+  , port                   :: Integer
+  , dbname                 :: String
+  , migrations_directory   :: Maybe String
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON DatabaseConfig
+instance FromJSON OnlyDatabaseConfig
+
+createConn :: DatabaseConfig -> IO Connection
+createConn config = do
+   catch (createConn' config)
+         (\(_::SomeException) -> do putStrLn "Failed to connecto to database, retrying in 10s.."
+                                    threadDelay $ 10 * 1000 * 1000
+                                    createConn' config)
+
+
+createConn' :: DatabaseConfig -> IO Connection
+createConn' config = do
+  connect ConnectInfo
+    { connectHost     = host     config
+    , connectUser     = user     config
+    , connectPassword = password config
+    , connectDatabase = dbname   config
+    , connectPort     = fromInteger $ port config
+    }
+
+destroyConn :: Connection -> IO ()
+destroyConn = close
