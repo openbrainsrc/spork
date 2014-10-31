408f6c6f52b985e496f0bf708993fd206c89492e
Tom Nielsen <tanielsen@gmail.com>
2014-10-31 09:02:10 +0000

    generic "is" for constructor matching

---
+++ spork-lib/Spork/Generics.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveDataTypeable #-}
+
+module Spork.Generics where
+
+import Data.Data
+
+data PhpValue = VoidValue | IntValue Integer | BoolValue Bool 
+              deriving (Typeable, Data)
+
+
+is :: Data a => a-> String -> Bool
+is x connm = connm == (showConstr $ toConstr x)
